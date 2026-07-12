9d748bf846dc428d33a986d9f8a19d633f57abaa
Smriti <Smritir225@gmail.com>
2026-07-12 11:30:18 +0530

    Day 16: Count frequency of elements using HashMap

---
+++ Day16.java
@@ -0,0 +1,41 @@
+// Problem: Given an array of integers, count the frequency of each distinct element and print the result.
+
+// Input:
+// - First line: integer n (size of array)
+// - Second line: n integers
+
+// Output:
+// - Print each element followed by its frequency in the format element:count
+
+// Example:
+// Input:
+// 5
+// 1 2 2 3 1
+
+// Output:
+// 1:2 2:2 3:1
+
+import java.util.*;
+
+public class Day16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.print(entry.getKey() + ":" + entry.getValue() + " ");
+        }
+    }
+}
