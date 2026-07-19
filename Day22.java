92da5ec1ec36b67474811c2b07ff8d8bf061e5cb
Smriti <Smritir225@gmail.com>
2026-07-19 10:49:35 +0530

    Day 22 - Count Nodes in Linked List

---
+++ Day22.java
@@ -0,0 +1,67 @@
+// Problem: Count Nodes in Linked List
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the result
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+
+// Output:
+// 10 20 30 40 50
+
+import java.util.Scanner;
+
+public class Day22 {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Node head = null;
+        Node tail = null;
+
+        // Create linked list
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        // Count nodes
+        int count = 0;
+        Node current = head;
+
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+    
+}
